963e1d74aac31704909301bb0b7c0134e152553b
Diana Tran <trandianat@gmail.com>
2024-04-01 17:15:35 -0400

    Scaffolded application

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { Amplify } from 'aws-amplify';
+// import awsconfig from 'aws-exports';
+import ReactDOM from 'react-dom';
+import 'styles.css';
+
+// Amplify.configure(awsconfig);
+
+ReactDOM.render(<h1>test</h1>, document.getElementById('root'));
